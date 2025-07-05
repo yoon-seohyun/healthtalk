d8efcbff845e5f27576274408e19ca74879dc8ef
yoon-seohyun <seohyun010206@nate.com>
2025-07-06 03:44:02 +0900

    Add new files

---
+++ src/main/java/com/healthtalk/healthtalk/HealthtalkApplication.java
@@ -0,0 +1,13 @@
+package com.healthtalk.healthtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthtalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthtalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthtalk/healthtalk/HealthtalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthtalk.healthtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthtalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
